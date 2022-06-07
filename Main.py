e7135da55e195e0196410ecc17225013d949e0a5
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-07 10:09:07 -0300

    first commit

---
+++ Main.py
@@ -0,0 +1,4 @@
+from Resource.Controllers.SistemaController import SistemaController
+
+if __name__ == "__main__":
+    SistemaController().iniciar()
---
+++ Resource/Controllers/SistemaController.py
@@ -0,0 +1,7 @@
+class SistemaController:
+
+    def __init__(self):
+        pass
+
+    def iniciar(self):
+        pass
