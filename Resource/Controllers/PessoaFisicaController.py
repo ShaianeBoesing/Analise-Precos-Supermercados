066f4f9cfc48be15235562e5a39ff076f6fa411c
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-10 16:43:40 -0300

    :rocket: Usuario -Implementaçao da estrutura base das classes de usuário (Pessoa Física e Jurídica_

---
+++ Resource/Controllers/PessoaFisicaController.py
@@ -0,0 +1,12 @@
+from Resource.util import Util
+
+
+class PessoaFisicaController():
+    def __init__(self):
+        pass
+
+    def logar(self):
+        pass
+
+    def criar_usuario(self):
+        pass
