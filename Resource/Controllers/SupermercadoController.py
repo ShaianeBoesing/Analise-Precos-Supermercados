2f8f5373684f7e6d4e4a8437b3561be25a30ce9f
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-09 23:48:52 -0300

    :rocket: inicialização do controlador do Supermercado (MVC). Corpo da classe

---
+++ Resource/Controllers/SupermercadoController.py
@@ -0,0 +1,27 @@
+from Resource.Models.Supermercado import *
+class SupermercadoController:
+
+    def __init__(self):
+        pass
+
+    def listar_supermercados(self):
+        pass
+
+    def criar_supermercado(self):
+        pass
+
+    def alterar_supermercado(self, supermercado):
+        pass
+
+    def excluir_supermercado(self, supermercado):
+        pass
+
+    def adicionar_supermercado_lista(self, supermercado):
+        pass
+
+    def remover_supermercado_lista(self, supermercado):
+        pass
+
+    def pesquisar_supermercado(self, supermercado):
+        pass
+
