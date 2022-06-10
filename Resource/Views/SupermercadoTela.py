a9cd921450980efd6781ddfac55d76f2388b5bc1
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-10 16:42:11 -0300

    :rocket: SupermercadoTela -Implementaçao do formulário para receber dados do supermercado -Implementaçao de método para exibir todos supermercados

--- Resource/Views/SupermercadoTela.py
+++ Resource/Views/SupermercadoTela.py
@@ -1,17 +1,24 @@
 from Resource.Controllers.SupermercadoController import *
-class TelaSupermercado:
+
+
+class SupermercadoTela:
 
     def __init__(self, controller):
         pass
 
     def cadastrar_supermercado_formulario(self):
-        pass
+        nome = input('Nome: ')
+        endereco = input('Endereco: ')
+        return {'nome': nome,
+                'endereco': endereco}
 
     def editar_supermercado_formulario(self):
         pass
 
-    def exibir_listas_supermercados(self):
-        pass
+
+    def exibir_listas_supermercados(self, supermercados: list):
+        for supermercado in supermercados:
+            print('- ', supermercado.nome, ' | ', supermercado.endereco)
 
     def exibir_infos_supermercado(self, Supermercado):
         pass
