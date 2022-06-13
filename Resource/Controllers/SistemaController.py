a436ef44872ab6cefa57daff52081f6a8a42fa35
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-13 13:35:44 -0300

    :rocket: Fluxo de inicialização do sistema

--- Resource/Controllers/SistemaController.py
+++ Resource/Controllers/SistemaController.py
@@ -1,3 +1,5 @@
+from Resource.Models.PessoaJuridica import PessoaJuridica
+from Resource.Models.PessoaFisica import PessoaFisica
 from Resource.Controllers.SupermercadoController import SupermercadoController
 from Resource.Controllers.PessoaJuridicaController import PessoaJuridicaController
 from Resource.Controllers.PessoaFisicaController import PessoaFisicaController
@@ -7,25 +9,53 @@ from Resource.Views.SistemaTela import SistemaTela
 class SistemaController:
 
     def __init__(self):
-        self.ON = True
+        self.__ON = True
+        self.__usuario_sessao = None
         self.__sistema_tela = SistemaTela()
         self.__supermercado_controller = SupermercadoController()
         self.__pessoas_juridica_controller = PessoaJuridicaController()
         self.__pessoas_fisica_controller = PessoaFisicaController()
-
-    def iniciar(self):
-        menu_opcoes = {
+        self.__menu_opcoes_acesso = {
             'Acessar como Pessoa Jurídica': self.__pessoas_juridica_controller.logar,
             'Acessar como Pessoa Física': self.__pessoas_fisica_controller.logar,
             'Cadastrar Pessoa Jurídica': self.__pessoas_juridica_controller.criar_usuario,
             'Cadastrar Pessoa Física': self.__pessoas_fisica_controller.criar_usuario,
             'Sair': self.sair
         }
+        self.__menu_opcoes_pessoa_juridica = {
+            'Ver supermercados': self.__supermercado_controller.listar_menus,
+            'Deslogar': self.deslogar,
+        }
+        self.__menu_opcoes_pessoa_fisica = {
+            'Deslogar': self.deslogar,
+        }
 
-        while self.ON:
-            opcao = self.__sistema_tela.ver_menu_inicial(menu_opcoes)
+    def iniciar(self):
+        self.__ON = True
+        menu_opcoes = self.__menu_opcoes_acesso
+        while self.__ON:
+            opcao = self.__sistema_tela.ver_menu(menu_opcoes)
+            if opcao:
+                usuario = menu_opcoes[opcao]()
+                self.__usuario_sessao = usuario
+                if isinstance(self.__usuario_sessao, PessoaJuridica):
+                    self.exibir_menu_sistema(self.__menu_opcoes_pessoa_juridica)
+                if isinstance(self.__usuario_sessao, PessoaFisica):
+                    self.exibir_menu_sistema(self.__menu_opcoes_pessoa_fisica)
+
+
+
+    def exibir_menu_sistema(self, menu_opcoes):
+        while self.__usuario_sessao:
+            opcao = self.__sistema_tela.ver_menu(menu_opcoes)
             if opcao:
                 menu_opcoes[opcao]()
+        else:
+            self.__sistema_tela.exibir_termino_sessao()
+
 
     def sair(self):
-        self.ON = False
+        self.__ON = False
+
+    def deslogar(self):
+        self.__usuario_sessao = None;
