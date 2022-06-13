ceca710b3b493dbf2c8b38b3500559adefabdc7c
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-13 13:32:35 -0300

    :rocket: Controlador de Pessoa Jurídica e Física - Implementação dos métodos criar e logar - Implementação de métodos obrigatórios: esqueleto

--- Resource/Controllers/PessoaFisicaController.py
+++ Resource/Controllers/PessoaFisicaController.py
@@ -7,3 +7,24 @@ class PessoaFisicaController():
 
     def criar_usuario(self):
         pass
+
+    def listar_usuarios(self):
+        pass
+
+
+    def alterar_usuario(self, usuario):
+        pass
+
+    def excluir_usuario(self, usuario):
+        pass
+
+
+    def verificar_usuario(self, usuario):
+        pass
+
+
+    def listar_menus(self):
+        pass
+
+    def adicionar_usuario_lista(self, usuario):
+        pass
