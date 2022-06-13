035c06c1aa2f484a1906c6a458a6d65e7a1713a2
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-13 13:32:35 -0300

    :rocket: Controlador de Pessoa Jurídica e Física - Implementação dos métodos criar e logar - Implementação de métodos obrigatórios: esqueleto

--- Resource/Controllers/PessoaJuridicaController.py
+++ Resource/Controllers/PessoaJuridicaController.py
@@ -1,9 +1,60 @@
-class PessoaJuridicaController():
+from Resource.Controllers.AbstractUsuarioController import AbstratcUsuarioController
+from Resource.Models.PessoaJuridica import PessoaJuridica
+from Resource.Views.PessoaJuridicaTela import PessoaJuridicaTela
+
+
+class PessoaJuridicaController(AbstratcUsuarioController):
     def __init__(self):
-        pass
+        self.__pessoa_juridica_tela = PessoaJuridicaTela()
+        self.__lista_pessoas_juridicas = []
+        self.__ON = True
+
 
     def logar(self):
-        pass
+        dados = self.__pessoa_juridica_tela.logar_formulario()
+
+        # Procura por nome e senha
+        for usuario in self.__lista_pessoas_juridicas:
+            if (usuario.nome == dados["nome"]) and (usuario.email == dados["email"]):
+                return usuario
+        else:
+            self.__pessoa_juridica_tela.mostra_mensagem("Nome ou Email inválidos!")
+            self.__pessoa_juridica_tela.continuar()
+            return None
 
     def criar_usuario(self):
+        dados = self.__pessoa_juridica_tela.cadastrar_usuario_formulario()
+        novo_usuario = PessoaJuridica(dados['nome'],
+                                      dados['email'],
+                                      dados['cnpj'])
+        self.adicionar_usuario_lista(novo_usuario)
+
+    def adicionar_usuario_lista(self, usuario):
+        if isinstance(usuario, PessoaJuridica) and \
+                (usuario not in self.__lista_pessoas_juridicas):
+            self.__lista_pessoas_juridicas.append(usuario)
+
+
+    def listar_usuarios(self):
         pass
+
+
+    def alterar_usuario(self, usuario):
+        pass
+
+    def excluir_usuario(self, usuario):
+        pass
+
+
+    def verificar_usuario(self, usuario):
+        pass
+
+
+    def listar_menus(self):
+        menu_opcoes = {
+            'Cadastrar Usuário': '',
+            'Voltar': self.voltar
+        }
+
+    def voltar(self):
+        self.__ON = False
