82547dbb2489b61cdd6050f4a0c8052c88cdaf88
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-13 13:35:44 -0300

    :rocket: Fluxo de inicialização do sistema

--- Resource/Views/SistemaTela.py
+++ Resource/Views/SistemaTela.py
@@ -4,20 +4,6 @@ class SistemaTela(AbstractTela):
     def __init__(self):
         pass
 
-    def ver_menu_inicial(self, opcoes):
-        menu_opcoes = [(item) for item in opcoes.keys()]
-        total = len(menu_opcoes)
-        for i in range(total):
-            print(i + 1, '-', menu_opcoes[i])
-
-        try:
-            opcao = int(input('Opção: ')) - 1
-            if opcao >= total:
-                raise ValueError(f'Valor maior que {i + 1}')
-
-        except ValueError:
-            print(f'O valor precisa ser um número inteiro entre 1 e {i + 1}')
-            super().continuar()
-            return False
-
-        return menu_opcoes[opcao]
+    def exibir_termino_sessao(self):
+        print('Sua sessão terminou!')
+        super().continuar()
