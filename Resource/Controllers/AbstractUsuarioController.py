d8474cca112cfb1860ac86b51af9a378b6abace5
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-13 13:28:11 -0300

    :rocket: Classe controlador de usuário abstrata - Nova classe abstrata para definir métodos obrigatórios dos controladores de usuário

---
+++ Resource/Controllers/AbstractUsuarioController.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+
+class AbstratcUsuarioController(ABC):
+
+    @abstractmethod
+    def listar_usuarios(self):
+        pass
+
+    @abstractmethod
+    def criar_usuario(self):
+        pass
+
+    @abstractmethod
+    def alterar_usuario(self, usuario):
+        pass
+
+    @abstractmethod
+    def excluir_usuario(self, usuario):
+        pass
+
+    @abstractmethod
+    def verificar_usuario(self, usuario):
+        pass
+
+    @abstractmethod
+    def adicionar_usuario_lista(self, usuario):
+        pass
+
+    @abstractmethod
+    def logar(self):
+        pass
+
+    @abstractmethod
+    def listar_menus(self):
+        pass
