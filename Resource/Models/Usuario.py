1ab5f029aea13097331812452687b29908199575
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-13 13:34:17 -0300

    :rocket: Usuario, Pessoa Jurídica e Física - Usuario - implementação de superclasse - Subclasses de usuário

---
+++ Resource/Models/Usuario.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class Usuario(ABC):
+    def __init__(self, nome, email):
+        self.__nome = nome
+        self.__email = email
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, email):
+        self.__email = email
+
