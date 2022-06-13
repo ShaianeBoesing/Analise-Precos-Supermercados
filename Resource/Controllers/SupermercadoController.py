95a35519350fa4333d8378d43fc6baf13f0da242
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-13 13:39:37 -0300

    :rocket: Controlador de Supermercado - Ajustes no fluxo de execução

--- Resource/Controllers/SupermercadoController.py
+++ Resource/Controllers/SupermercadoController.py
@@ -5,8 +5,15 @@ from Resource.Views.SupermercadoTela import SupermercadoTela
 class SupermercadoController:
 
     def __init__(self):
+        self.__ON = True
         self.__lista_supermercados = []
         self.__tela_supermercado = SupermercadoTela(self)
+        self.__menu_opcoes = {
+                'Cadastrar Supermercado': self.criar_supermercado,
+                'Listar Supermercados': self.listar_supermercados,
+                'Voltar': self.voltar
+            }
+
 
     def listar_supermercados(self):
         self.__tela_supermercado.exibir_listas_supermercados(self.__lista_supermercados)
@@ -18,7 +25,7 @@ class SupermercadoController:
             dados_supermercado['endereco']
         )
         self.adicionar_supermercado_lista(novo_supermercado)
-        self.listar_supermercados()
+        self.__tela_supermercado.exibir_mensagem("Supermercado cadastrado com sucesso!")
 
     def alterar_supermercado(self, supermercado):
         pass
@@ -36,3 +43,16 @@ class SupermercadoController:
 
     def pesquisar_supermercado(self, supermercado):
         pass
+
+    def listar_menus(self):
+        self.__ON = True
+        while self.__ON:
+            menu_opcoes = self.__menu_opcoes
+            opcao = self.__tela_supermercado.ver_menu(menu_opcoes)
+            if opcao:
+                menu_opcoes[opcao]()
+
+
+
+    def voltar(self):
+        self.__ON = False
