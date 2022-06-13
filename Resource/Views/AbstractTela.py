e7d107e099ecf38d7c0692cb1d22fb5135e28f62
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-13 13:29:50 -0300

    :rocket: Tela Abstrata - Novos métodos na classe AbstractTela - mostra_mensagem - exibe mensagem personalizada que recebe no parametro - ver_menu - exibe o dicionário passado como parâmetro em forma de menu e retorna a opção selecionada

--- Resource/Views/AbstractTela.py
+++ Resource/Views/AbstractTela.py
@@ -5,3 +5,24 @@ class AbstractTela(ABC):
 
     def continuar(self):
         input('Pressione <ENTER> para continuar')
+
+    def mostra_mensagem(self, mensagem):
+        print(mensagem)
+
+    def ver_menu(self, opcoes):
+        menu_opcoes = [(item) for item in opcoes.keys()]
+        total = len(menu_opcoes)
+        for i in range(total):
+            print(i + 1, '-', menu_opcoes[i])
+
+        try:
+            opcao = int(input('Opção: ')) - 1
+            if opcao >= total:
+                raise ValueError(f'Valor maior que {i + 1}')
+
+        except ValueError:
+            print(f'O valor precisa ser um número inteiro entre 1 e {i + 1}')
+            super().continuar()
+            return False
+
+        return menu_opcoes[opcao]
