cb0218a66f29d19fcfb737306bb3a6243bb0a816
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-14 13:43:07 -0300

    :hammer: Pessoa jurídica - Formulário de criação de pessoa jurídica com opções de supermercado.

--- Resource/Views/PessoaJuridicaTela.py
+++ Resource/Views/PessoaJuridicaTela.py
@@ -11,11 +11,28 @@ class PessoaJuridicaTela(AbstractTela):
         data = {'nome': nome, 'email': email}
         return data
 
-    def cadastrar_usuario_formulario(self):
-        nome = input('Nome: ')
-        email = input('Email: ')
-        cnpj = input('CNPJ: ')
-        data = {'nome': nome,
-                'email': email,
-                'cnpj': cnpj}
-        return data
+    def cadastrar_usuario_formulario(self, supermercados):
+        try:
+            nome = input('Nome: ')
+            email = input('Email: ')
+            cnpj = input('CNPJ: ')
+            self.opcoes_supermercados(supermercados)
+            supermercado = int(input('Supermercado: '))
+            if supermercado > len(supermercados):
+                raise ValueError(f'Valor maior que {len(supermercados)}')
+
+            data = {'nome': nome,
+                    'email': email,
+                    'cnpj': cnpj,
+                    'supermercado': supermercado
+                    }
+            return data
+        except ValueError:
+            print(f'O valor precisa ser um número inteiro entre 1 e {len(supermercados)}')
+            self.continuar()
+            return False
+
+    def opcoes_supermercados(self, supermercados):
+        super().exibir_mensagem("Escolha uma opção")
+        for i in range(len(supermercados)):
+            print(i+1,'- ', supermercados[i].nome, ' | ', supermercados[i].endereco)
