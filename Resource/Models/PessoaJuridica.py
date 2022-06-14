d8abc50bc69aca09aa330d4657ce3aeacec522cd
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-14 13:39:36 -0300

    :hammer: AbstractTela - Alterando regra de associação e associando supermercado à pessoa jurídica. - Agora, a pessoa jurídica está ligada a um supermercado

--- Resource/Models/PessoaJuridica.py
+++ Resource/Models/PessoaJuridica.py
@@ -1,6 +1,19 @@
 from Resource.Models.Usuario import Usuario
+
+
 class PessoaJuridica(Usuario):
 
-    def __init__(self, nome, email, cnpj):
+    def __init__(self, nome, email, cnpj, supermercado):
         super().__init__(nome, email)
         self.__cnpj = cnpj
+        self.__supermercado = supermercado
+
+    @property
+    def supermercado(self):
+        return self.__supermercado
+
+    @supermercado.setter
+    def supermercado(self, supermercado):
+        self.__supermercado = supermercado
+        print((self.supermercado).nome)
+
