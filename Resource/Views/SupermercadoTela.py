5d36d5fa023858e909b12e1190597e396a093d7f
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-14 14:12:15 -0300

    :hammer: Escolher Supermercado

--- Resource/Views/SupermercadoTela.py
+++ Resource/Views/SupermercadoTela.py
@@ -19,10 +19,21 @@ class SupermercadoTela(AbstractTela):
 
     def exibir_listas_supermercados(self, supermercados: list):
         super().exibir_mensagem("Lista de Supermercados")
-        for supermercado in supermercados:
-            print('- ', supermercado.nome, ' | ', supermercado.endereco)
-        super().continuar()
+        for i in range(len(supermercados)):
+            print(i + 1, '- ', supermercados[i].nome, ' | ', supermercados[i].endereco)
+
+    def escolher_supermercado(self, supermercados):
+        try:
+            self.exibir_listas_supermercados(supermercados)
+            total = len(supermercados)
+            opcao = int(input('Opção: '))
+            if opcao-1 > total:
+                raise ValueError(f'Valor maior que {total}')
+            return opcao
+        except ValueError:
+            print(f'O valor precisa ser um número inteiro entre 1 e {total}')
+            self.continuar()
+            return False
 
     def exibir_infos_supermercado(self, Supermercado):
         pass
-
