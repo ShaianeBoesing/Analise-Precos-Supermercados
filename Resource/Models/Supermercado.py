882bd33c147fca5cc5071700234501e4925a98e5
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-14 14:11:37 -0300

    :hammer: Removendo funcionários de supermercado

--- Resource/Models/Supermercado.py
+++ Resource/Models/Supermercado.py
@@ -5,7 +5,6 @@ class Supermercado:
     def __init__(self, nome: str, endereco: str):
         self.__nome = nome
         self.__endereco = endereco
-        self.__funcionarios = []
 
     @property
     def nome(self):
@@ -23,15 +22,3 @@ class Supermercado:
     def endereco(self, endereco):
         self.__endereco = endereco
 
-    @property
-    def funcionarios(self):
-        return self.__funcionarios
-
-    def adicionar_funcionario_lista(self, funcionario):
-        if isinstance(funcionario, PessoaJuridica):
-            if funcionario not in self.__funcionarios:
-                self.__funcionarios.append(funcionario)
-
-    def remover_funcionario_lista(self, funcionario):
-        if funcionario in self.__funcionarios:
-            self.__funcionarios.remove(funcionario)
