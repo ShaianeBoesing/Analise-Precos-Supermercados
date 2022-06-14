2fdd784be13bf7b45b0935252e8da82510eab64f
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-14 13:45:43 -0300

    :hammer: Classe, Controlador e Tela de Preço - Método para retornar os preços filtrados por supermercado

---
+++ Resource/Models/Preco.py
@@ -0,0 +1,59 @@
+class Preco:
+    def __init__(self, valor, data, produto, qualificadores, usuario, supermercado):
+        self.__valor = valor
+        self.__data = data
+        self.__contador = 0
+        self.__produto = produto
+        self.__supermercado = supermercado
+        self.__qualificadores = qualificadores
+        self.__usuario = usuario
+
+    @property
+    def valor(self):
+        return self.__valor
+
+    @valor.setter
+    def valor(self, valor):
+        self.__valor = valor
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, data):
+        self.__data = data
+
+    @property
+    def produto(self):
+        return self.__produto
+
+    @produto.setter
+    def produto(self, produto):
+        self.__produto = produto
+
+    @property
+    def supermercado(self):
+        return self.__supermercado
+
+    @supermercado.setter
+    def supermercado(self, supermercado):
+        self.__supermercado = supermercado
+
+    @property
+    def usuario(self):
+        return self.__usuario
+
+    @usuario.setter
+    def usuario(self, usuario):
+        self.__usuario = usuario
+
+    @property
+    def qualificadores(self):
+        return self.__qualificadores
+
+    @qualificadores.setter
+    def qualificadores(self, qualificadores):
+        self.__qualificadores = qualificadores
+
+
