a582101d310d37b10271a9617dc4680e0985f4c0
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-06-20 08:08:35 -0300

    :rocket: Preço - implementação da lógica de criar preço e avaliar produtos (confirma_preco)

--- Resource/Controllers/PrecoController.py
+++ Resource/Controllers/PrecoController.py
@@ -1,12 +1,14 @@
 from Resource.Models.Preco import Preco
 from Resource.Views.PrecoTela import PrecoTela
+from datetime import datetime
+
 
 
 class PrecoController:
     def __init__(self, sistema):
         self.__lista_precos = []
         self.__sistema = sistema
-        self.__tela_preco = PrecoTela()
+        self.__tela_preco = PrecoTela(self)
 
     #  GETTERS
     @property
@@ -22,35 +24,44 @@ class PrecoController:
         return self.__tela_preco
 
     # CRUD
-    def criar_preco(self):
+    def criar_preco(self, produto):
         self.__tela_preco.exibir_mensagem('FORMULÁRIO DE PREÇO: ')
         dados_preco = self.__tela_preco.cadastrar_preco_formulario()
-        novo_preco = Preco(
-            dados_preco['valor'],
-            dados_preco['data'],
-            dados_preco['produto'],
-            dados_preco['supermercado'],
-            dados_preco['qualificadores'],
-            dados_preco['usuario'],
-            dados_preco['contador']
-        )
-        self.adicionar_preco_lista(novo_preco)
-        self.__tela_preco.exibir_mensagem("Produto cadastrado com sucesso!")
-        self.__tela_preco.continuar()
+        if dados_preco:
+            dados_preco['usuario'] = self.__sistema.usuario_sessao
+            dados_preco['data'] = datetime.today().strftime('%d/%m/%Y')
+            dados_preco['produto'] = produto
+            dados_preco['qualificadores'] = produto.qualificadores
+            dados_preco['contador'] = 1
+            novo_preco = Preco(
+                dados_preco['valor'],
+                dados_preco['data'],
+                dados_preco['produto'],
+                dados_preco['qualificadores'],
+                dados_preco['usuario'],
+                dados_preco['supermercado']
+            )
+            self.adicionar_preco_lista(novo_preco)
+            self.__tela_preco.exibir_mensagem('Preço informado com sucesso!')
+            self.__tela_preco.continuar()
+            return novo_preco
+        return False
 
-    def listar_precos(self):
-        self.__tela_preco.exibir_lista_precos(self.__lista_precos)
+    def listar_precos(self, lista_precos = None):
+        if not lista_precos:
+            lista_precos = self.__lista_precos
+        self.__tela_preco.exibir_lista_precos(lista_precos)
         self.__tela_preco.continuar()
 
     def alterar_preco(self):
         pass
 
     def excluir_preco(self):
-        preço = self.escolher_preco()
-        if preço:
+        preco = self.escolher_preco()
+        if preco:
             confirma = self.__tela_preco.exibir_confirmacao_exclusao()
             if confirma:
-                self.remover_preço_lista(preço)
+                self.remover_preco_lista(preco)
                 self.__tela_preco.exibir_mensagem('Preço excluído com sucesso!')
                 self.__tela_preco.continuar()
 
@@ -66,25 +77,25 @@ class PrecoController:
         return precos_supermercado
 
     def escolher_preco(self):
-        preços = self.__lista_precos
-        opcao = self.__tela_preco.escolher_produto(preços)
+        precos = self.__lista_precos
+        opcao = self.__tela_preco.escolher_produto(precos)
         if opcao != False:
-            return preços[opcao - 1]
+            return precos[opcao - 1]
         return False
 
-    def adicionar_preco_lista(self, preço):
-        if isinstance(preço, Preco):
-            if preço in self.__lista_precos:
-                self.__lista_precos.append(preço)
+    def adicionar_preco_lista(self, preco):
+        if isinstance(preco, Preco):
+            if preco in self.__lista_precos:
+                self.__lista_precos.append(preco)
 
-    def remover_preço_lista(self, preço):
-        if isinstance(preço, Preco):
-            if preço in self.__lista_precos:
-                self.__lista_precos.remove(preço)
+    def remover_preco_lista(self, preco):
+        if isinstance(preco, Preco):
+            if preco in self.__lista_precos:
+                self.__lista_precos.remove(preco)
 
-    def incrementar_contador(self, preço):
-        if isinstance(preço, Preco):
-            if preço in self.__lista_precos:
+    def incrementar_contador(self, preco):
+        if isinstance(preco, Preco):
+            if preco in self.__lista_precos:
                 self.__contador += 1
             else:
                 self.__contador = 1
