4e2513131d1b750c24bae90c36e05ed74b273f3f
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-07-25 09:29:07 -0300

    :lipstick: feat: adiciona interface GUI para exibir relatórios

--- Resource/Views/RelatoriosTela.py
+++ Resource/Views/RelatoriosTela.py
@@ -1,3 +1,4 @@
+import PySimpleGUI as sg
 from Resource.Views.AbstractTela import AbstractTela
 
 class RelatoriosTela(AbstractTela):
@@ -5,8 +6,17 @@ class RelatoriosTela(AbstractTela):
         pass
 
     def exibir_produto_por_supermercado(self, dicionario):
-        self.exibir_mensagem('RELATÓRIO DE PRODUTOS POR SUPERMERCADO')
-        for mercado, lista_produtos in dicionario.items():
-            print(f'SUPERMERCADO {mercado.nome}')
-            for prod in lista_produtos:
-                print(f'- {prod.nome}')
+        if dicionario.items():
+            layout = [[sg.Text('RELATÓRIO DE PRODUTOS POR SUPERMERCADO')]]
+            for mercado, lista_produtos in dicionario.items():
+                layout.append([sg.Text(mercado.nome)])
+                for prod in lista_produtos:
+                    layout.append([sg.Text('- ' + prod.nome)])
+
+            layout.append([sg.Button('OK')])
+            self.window = sg.Window('Cadastro Categoria').Layout(layout)
+
+            button, response = self.open()
+            self.close()
+        else:
+            self.exibir_mensagem('Não foi possível gerar seu relatório')
