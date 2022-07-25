503bbe0c5efc600153861e64b8c464d2904af079
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-07-24 23:17:44 -0300

    :fire: fix: remove pasta inutilizada

--- Resource/Interface Gráfica/RelatoriosTela.py
+++
@@ -1,9 +0,0 @@
-import PySimpleGUI as sg
-
-class RelatoriosTela():
-    def __init__(self):
-        self.__window = None
-        self.exibir_relatorio()
-
-    def exibir_relatorio(self, dicionario):
-        pass
